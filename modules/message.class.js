d1d74f4d06c95816802dc7fd7147147dff5a5fc3
OlaJFrick <info@culturehero.org>
2017-02-21 00:01:14 +0100

    Added class Message Schema

---
+++ modules/message.class.js
@@ -0,0 +1,12 @@
+module.exports = class Message {
+
+	schema() {
+	    return {
+			username: {type: String, required: true},
+			title: {type: String, required: true},
+			message: {type: String, required: true},
+			date: {type: String, required: true}
+    	};
+	}
+
+};
